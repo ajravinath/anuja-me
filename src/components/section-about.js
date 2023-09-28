44e6f37a4f0705505de35fb968d48353cff796bb
Anuja <anuja@calcey.com>
2023-09-29 03:31:35 +0530

    update project section

--- src/components/section-about.js
+++ src/components/section-about.js
@@ -9,45 +9,47 @@ const SectionAbout = () => {
     <section id="about">
       <p class="section__text__p1">Get to know more</p>
       <h1 class="title">About Me</h1>
-      <div class="section-container">
-        <div class="section__pic-container">
-          <img src="https://drive.google.com/uc?export=view&id=1l6RG5CMP5sE5XpYI5KJ7E6_vvEyqbBo3" alt="profile picture" class="about-pic" />
-        </div>
-        <div class="about-details-container">
-          <div class="about-containers">
-            <div class="details-container">
-              <div>
-                <img src="./experience.png" alt="experience icon" class="icon" />
-                <h3>Experience</h3>
-                <p><b>${window.calcXp()}+ years</b><br> Software Engineering</p>
-              </div>
+        <div class="experience-details-container">
+          <div class="about-containers xp">
+            <div class="section__pic-container banner-pic">
+              <img src="https://drive.google.com/uc?export=view&id=1l6RG5CMP5sE5XpYI5KJ7E6_vvEyqbBo3" alt="profile picture" class="about-pic" />
             </div>
-            <div class="details-container">
-              <div>
-                <img src="./education.png" alt="education icon" class="icon" />
-                <h3>Education</h3>
-                <p>BSc. Computer Science<br>General Sir John Kotelawala Defence University<br><b>Upper Second Class Honours (GPA 3.64)</b>
+            <div class="about-details-container">
+              <div class="about-containers">
+                <div class="details-container">
+                  <div>
+                    <img src="./experience.png" alt="experience icon" class="icon" />
+                    <h3>Experience</h3>
+                    <p><b>${window.calcXp()}+ years</b><br> Software Engineering</p>
+                  </div>
+                </div>
+                <div class="details-container">
+                  <div>
+                    <img src="./education.png" alt="education icon" class="icon" />
+                    <h3>Education</h3>
+                    <p>BSc. Computer Science<br>General Sir John Kotelawala Defence University<br><b>Upper Second Class Honours (GPA 3.64)</b>
+                    </p>
+                  </div>
+                </div>
+              </div>
+              <div class="text-container">
+                <p>
+                <div class="about-caption">B.Sc. in Computer Science | <span class="year">2014 - 2018</span></div>
+                4th year graduate with second class upper division honors. Including core programs such as Computational Mathematics, Web Development, Data Structures and Algorithms, Machine Learning, Natural Language Processing, Data Mining, Image Processing etc.
+                <hr/>
+                <div class="about-caption">6+ years of experience in Software Engineering</div>
+                <span>Software Engineer 2 | Glints Pte Ltd | <span class="year">2022 - Present</span></span><br>
+                <span>Senior Software Engineer | Wiley Global Technologies | <span class="year">2021 - 2022</span></span><br>
+                <span>Senior Software Engineer | Calcey Technologies | <span class="year">2021</span></span><br>
+                <span>Software Engineer | Calcey Technologies | <span class="year">2018 - 2021</span></span><br>
+                <span>Associate Software Engineer | Calcey Technologies | <span class="year">2017 - 2018</span></span><br>
+                <span>Intern Software Engineer | Visni Pvt Ltd | <span class="year">2017</span></span><br>
                 </p>
               </div>
             </div>
-          </div>
-          <div class="text-container">
-            <p>
-            <div class="about-caption">B.Sc. in Computer Science | <span class="year">2014 - 2018</span></div>
-            4th year graduate with second class upper division honors. Including core programs such as Computational Mathematics, Web Development, Data Structures and Algorithms, Machine Learning, Natural Language Processing, Data Mining, Image Processing etc.
-            <hr/>
-            <div class="about-caption">6+ years of experience in Software Engineering</div>
-            <span>Software Engineer 2 | Glints Pte Ltd | <span class="year">2022 - Present</span></span><br>
-            <span>Senior Software Engineer | Wiley Global Technologies | <span class="year">2021 - 2022</span></span><br>
-            <span>Senior Software Engineer | Calcey Technologies | <span class="year">2021</span></span><br>
-            <span>Software Engineer | Calcey Technologies | <span class="year">2018 - 2021</span></span><br>
-            <span>Associate Software Engineer | Calcey Technologies | <span class="year">2017 - 2018</span></span><br>
-            <span>Intern Software Engineer | Visni Pvt Ltd | <span class="year">2017</span></span><br>
-            </p>
+            <img src="./arrow.png" alt="arrow icon" class="icon arrow" onclick="location.href='./#experience'" />
           </div>
         </div>
-        <img src="./arrow.png" alt="arrow icon" class="icon arrow" onclick="location.href='./#experience'" />
-      </div>
     </section>
     `;
 
